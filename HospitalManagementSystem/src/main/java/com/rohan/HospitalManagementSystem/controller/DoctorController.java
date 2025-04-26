8a699ef5e69643383ebb92e88b915856b8014546
Muhammad-Rohan <bscs2380246@szabist.pk>
2025-04-26 20:48:18 +0500

    three entities added
    Doctors, Patients, Appointments

---
+++ HospitalManagementSystem/src/main/java/com/rohan/HospitalManagementSystem/controller/DoctorController.java
@@ -0,0 +1,31 @@
+package com.rohan.HospitalManagementSystem.controller;
+
+import com.rohan.HospitalManagementSystem.entity.Doctors;
+import com.rohan.HospitalManagementSystem.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+
+    @Autowired
+    public DoctorService doctorService;
+
+    @GetMapping("/all")
+    public List<Doctors> getAllDoctors() {
+        return doctorService.getAllDoctors();
+    }
+
+    @GetMapping("/doctor/{id}")
+    public Optional<Doctors> getDoctorByID(@PathVariable int id) {
+        return doctorService.getDoctorByID(id);
+    }
+
+    
+
+
+}
